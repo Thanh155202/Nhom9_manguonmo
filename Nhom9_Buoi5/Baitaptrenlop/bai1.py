a7e6404a690cb34b3d2ba476e752e426287a1884
PMSang-2k2 <sangpham617102@gmail.com>
2023-10-10 21:44:34 +0700

    them phan chuan hoa DL va chia DL thanh tap huan luyen và tap ktra

--- Nhom9_Buoi5/Baitaptrenlop/bai1.py
+++ Nhom9_Buoi5/Baitaptrenlop/bai1.py
@@ -10,4 +10,9 @@ data = pd.read_csv('Student_Performance.csv')
 X = data.drop('Performance Index', axis=1)
 y = data['Performance Index']
 
+# Chuẩn hóa dữ liệu
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
 
+# Chia dữ liệu thành tập huấn luyện và tập kiểm tra
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
