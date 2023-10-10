6558f4021f0ef7835c824ea55bd5355f23e8cea4
vuthienhoang <vu171202@gmail.com>
2023-10-10 21:37:21 +0700

    sua doc chia du lieu

--- Nhom9_Buoi5/Baitaptrenlop/bai1.py
+++ Nhom9_Buoi5/Baitaptrenlop/bai1.py
@@ -10,15 +10,4 @@ data = pd.read_csv('Student_Performance.csv')
 X = data.drop('Performance Index', axis=1)
 y = data['Performance Index']
 
-# Chuẩn hóa dữ liệu
-scaler = MinMaxScaler()
-X = scaler.fit_transform(X)
 
-# Chia dữ liệu thành tập huấn luyện và tập kiểm tra
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Xây dựng mô hình
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
-    tf.keras.layers.Dense(1)
-])
