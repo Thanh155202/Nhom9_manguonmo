97f29bb4f40cb6002eca0b8a441948313a220094
vuthienhoang <vu171202@gmail.com>
2023-10-10 21:23:54 +0700

    doc chia du lieu

---
+++ Nhom9_Buoi5/Baitaptrenlop/bai1.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+# Đọc dữ liệu từ tệp Excel
+data = pd.read_csv('Student_Performance.csv')
+
+# Chia dữ liệu thành các đặc trưng và nhãn
+X = data.drop('Performance Index', axis=1)
+y = data['Performance Index']
+
+# Chuẩn hóa dữ liệu
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+# Chia dữ liệu thành tập huấn luyện và tập kiểm tra
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Xây dựng mô hình
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
+    tf.keras.layers.Dense(1)
+])
