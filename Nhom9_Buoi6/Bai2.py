6a11359700bca214ef7656bbf53d568cca19a263
Thanh155202 <Nguyenthanh155202@gmail.com>
2023-10-17 21:21:26 +0700

    Bai2 full

--- Nhom9_Buoi6/Bai2.py
+++ Nhom9_Buoi6/Bai2.py
@@ -14,7 +14,6 @@ def update_image():
     img_label.config(image=img)
     img_label.image = img
 
-
 # Hàm xử lý sự kiện khi nút "Chọn ảnh" được nhấn
 def select_image():
     global original_img, current_scale, current_rotation
@@ -24,3 +23,42 @@ def select_image():
         current_scale = 1.0
         current_rotation = 0
         update_image()
+# Hàm xử lý sự kiện khi nút "Xác nhận Góc Quay" được nhấn
+def change_rotation():
+    global current_rotation
+    rotation_text = rotation_entry.get()
+    try:
+        current_rotation = float(rotation_text)
+        update_image()
+    except ValueError:
+        pass
+
+# Tạo cửa sổ
+window = tk.Tk()
+window.title("Xoay ảnh")
+
+# Khai báo biến toàn cục
+current_scale = 1.0
+current_rotation = 0
+original_img = None
+scale_factor = 1.2
+
+# Tạo nút "Chọn ảnh" và đặt sự kiện khi nút này được nhấn
+select_button = tk.Button(window, text="Chọn ảnh", command=select_image)
+select_button.pack()
+
+
+# Tạo hộp văn bản để nhập góc quay
+rotation_label = tk.Label(window, text="Nhập góc quay (độ):")
+rotation_label.pack()
+rotation_entry = tk.Scale(window, from_=0, to=360, orient="horizontal")
+rotation_entry.pack()
+rotation_confirm_button = tk.Button(window, text="Xác nhận Góc Quay", command=change_rotation)
+rotation_confirm_button.pack()
+
+
+# Tạo label để hiển thị ảnh
+img_label = tk.Label(window)
+img_label.pack()
+
+window.mainloop()
