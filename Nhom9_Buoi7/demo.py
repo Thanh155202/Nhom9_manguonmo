9c8f764c0f4c935420ec46aca88fa7028fc8a733
kinhka <trongkinhslp2002@gmail.com>
2023-10-23 20:39:33 +0700

    update demo

--- Nhom9_Buoi7/demo.py
+++ Nhom9_Buoi7/demo.py
@@ -2,76 +2,123 @@ import cv2
 import os
 import hashlib
 from datetime import datetime
+import tkinter as tk
 
-# Tạo một thư mục để lưu ảnh khuôn mặt
-output_folder = 'faces'
-os.makedirs(output_folder, exist_ok=True)
 
-# Tạo một danh sách để lưu hash của khuôn mặt đã phát hiện
-detected_faces_hash = []
+# Tạo giao diện GUI
+root = tk.Tk()
+root.title("Ứng dụng nhận diện khuôn mặt")
+root.geometry("800x450")  # Set the initial window size
 
-# Tạo một đối tượng Cascade Classifier với tệp xml của Haar Cascade cho khuôn mặt
-face_cascade = cv2.CascadeClassifier('nhan_dien.xml')
+# Thêm hình nền
+hinh_nền = tk.PhotoImage(file="b.png")
+background_label = tk.Label(root, image=hinh_nền)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# Bật máy ảnh (thường là máy ảnh được tích hợp sẵn trên máy tính)
-cap = cv2.VideoCapture(0)
+# Thực hiện các chức năng
+def nhandang():
+    def nhandang_luu():
+        # Tạo thư mục lưu
+        output_folder = 'faces'
+        os.makedirs(output_folder, exist_ok=True)
 
-# Sử dụng biến để theo dõi xem mỗi khuôn mặt đã được lưu hay chưa
-face_saved = False
+        # tạo danh sách trống
+        ds_trong = []
 
-while True:
-    # Đọc video từ máy ảnh
-    ret, frame = cap.read()
+        # Tạo đối tượng Cascade Classifier bằng tệp XML Haar Cascade để nhận diện khuôn mặt
+        face_cascade = cv2.CascadeClassifier('nhandien.xml')
 
-    if not ret:
-        break
+        # mở camera
+        cap = cv2.VideoCapture(0)
 
-    # Chuyển hình ảnh sang màu xám để dễ dàng xử lý
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Sử dụng Cascade Classifier để xác định khuôn mặt trong hình ảnh
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    # Lưu ảnh khuôn mặt không trùng lặp với tên bao gồm thời gian hiện tại
-    if not face_saved and len(faces) > 0:
-        for i, (x, y, w, h) in enumerate(faces):
-            face_image = frame[y:y + h, x:x + w]
-
-            # Tính hash của ảnh khuôn mặt để kiểm tra trùng lặp
-            hash_value = hashlib.md5(face_image.tobytes()).hexdigest()
-
-            # Kiểm tra xem khuôn mặt đã tồn tại trong danh sách hay chưa
-            if hash_value not in detected_faces_hash:
-                detected_faces_hash.append(hash_value)  # Thêm hash vào danh sách
-
-                # Lấy thời gian hiện tại
-                current_time = datetime.now()
-                current_time_str = current_time.strftime('%Y%m%d%H%M%S')  # Định dạng ngày tháng giờ phút giây
-
-                # Tạo tên tệp bằng cách kết hợp thời gian và số thứ tự
-                image_filename = os.path.join(output_folder, f'face_{current_time_str}_{i}.jpg')
-                cv2.imwrite(image_filename, face_image)
-
-                face_saved = True  # Đánh dấu rằng đã lưu ảnh cho khuôn mặt hiện tại
-
-    # Vẽ khung xung quanh khuôn mặt và hiển thị số lượng khuôn mặt đã phát hiện
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-    # Hiển thị số khuôn mặt được phát hiện
-    face_count = len(faces)
-    cv2.putText(frame, f'So khuon mat : {face_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-    # Hiển thị video đầu ra
-    cv2.imshow('Nhận diện khuon mat', frame)
-
-    # Đặt biến `face_saved` lại thành False để cho phép lưu ảnh cho khuôn mặt tiếp theo
-    if face_saved and len(faces) == 0:
+        # Sử dụng một biến để theo dõi xem từng khuôn mặt đã được lưu hay chưa
         face_saved = False
 
-    # Thoát khỏi vòng lặp khi người dùng nhấn phím 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        while True:
+            # đọc video từ camera
+            ret, frame = cap.read()
 
-# Giải phóng tài nguyên
-cap.release()
-cv2.destroyAllWindows()
+            if not ret:
+                break
+
+            # Chuyển đổi hình ảnh sang thang độ xám để xử lý dễ dàng
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # sử dụng  Cascade Classifier để nhận diện khuôn mặt
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+            # Lưu hình ảnh khuôn mặt không trùng lặp với tên chứa dấu thời gian
+            if not face_saved and len(faces) > 0:
+                for i, (x, y, w, h) in enumerate(faces):
+                    face_image = frame[y:y + h, x:x + w]
+
+                    # Tính giá trị hash của hình ảnh khuôn mặt để kiểm tra sự trùng lặp
+                    hash_value = hashlib.md5(face_image.tobytes()).hexdigest()
+
+                    # kiem tra nếu khuôn mặt chưa xh trong list
+                    if hash_value not in ds_trong:
+                        ds_trong.append(hash_value)
+
+                        #Lấy thời gian thuc
+                        current_time = datetime.now()
+                        current_time_str = current_time.strftime('%Y%m%d%H%M%S')
+
+                        # Tạo tên tệp bằng cách kết hợp dấu thời gian và chỉ mục
+                        image_filename = os.path.join(output_folder, f'face_{current_time_str}_{i}.jpg')
+                        cv2.imwrite(image_filename, face_image)
+
+                        face_saved = True  # Đánh dấu ảnh đã được lưu cho khuôn mặt hiện tại
+
+            # Vẽ hình chữ nhật xung quanh các khuôn mặt được phát hiện và hiển thị số lượng khuôn mặt được phát hiện
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+            # hiển thị số khuôn mặt được phát hiện
+            face_count = len(faces)
+            cv2.putText(frame, f'So khuon mat duoc phat hien: {face_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+            # video ra
+            cv2.imshow('FACE DETECTION', frame)
+
+            # Đặt biến `face_saved` về Sai để cho phép lưu ảnh cho khuôn mặt tiếp theo
+            if face_saved and len(faces) == 0:
+                face_saved = False
+
+            # thoát chương trình
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Giải phóng tài nguyên
+        cap.release()
+        cv2.destroyAllWindows()
+
+    # Bắt đầu quá trình nhận diện khuôn mặt trong một qt  mới
+    import threading
+    threading.Thread(target=nhandang_luu).start()
+
+# Mở folder chứa gương mặt được nhận dạng
+def mo_tmuc():
+    folder_path = os.path.abspath('faces')
+    os.system(f'explorer {folder_path}')  
+
+# Thoát chương trình
+def thoat():
+    root.destroy()
+
+# tạo tiêu đề
+title_label = tk.Label(root, text="ỨND DỤNG NHẬN DIỆN KHUÔN MẶT", font=("Comic Sans MS", 25))
+title_label.grid(row=0, column=0, columnspan=3, pady=20)  # Place the title label in a single row
+
+# tạo
+start_button = tk.Button(root, text="Bắt đầu nhận diện", command=nhandang,bg="green",fg="white",font=("Comic Sans MS", 20))
+open_folder_button = tk.Button(root, text="Mở thư mục ảnh", command=mo_tmuc,bg="lavender", font=("Comic Sans MS", 20))
+exit_button = tk.Button(root, text="Thoát chương trình", command=thoat,bg="red",fg="white", font=("Comic Sans MS", 20))
+
+start_button.grid(row=1, column=0, pady=20, padx=10)
+open_folder_button.grid(row=1, column=1, pady=20, padx=10)
+exit_button.grid(row=1, column=2, pady=20, padx=10)
+
+# thoát chương trình
+root.bind('q', thoat)
+
+root.mainloop()
